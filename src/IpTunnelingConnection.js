86a5d69213ce6f3f4a9a858faafb7ca9d0bd566a
Sophia <volodfree34@gmail.com>
2021-12-04 10:19:07 +0300

    restoring cloned repo for followers...

--- src/IpTunnelingConnection.js
+++ src/IpTunnelingConnection.js
@@ -5,7 +5,6 @@
 
 const dgram = require('dgram');
 const KnxLog = require('./KnxLog.js');
-const tcpnet=require('net');
 
 function IpTunnelingConnection(instance) {
   const log = KnxLog.get();
@@ -25,14 +24,6 @@ function IpTunnelingConnection(instance) {
 
   instance.Connect = function () {
     this.localAddress = this.getLocalAddress();
-    // todo: evaluate the connection_type
-
-    // create tcp connection
-    this.client=tcpnet.createConnection({port:8124}, ()=>{
-      this.transition('connecting');
-
-    });
-
     // create the socket
     this.socket = this.BindSocket((socket) => {
       socket.on('error', (errmsg) => log.debug('Socket error: %j', errmsg));
